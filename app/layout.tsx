2d200227572b52150a8baa01dda09169bafc5349
Aayush <aausgh@gmail.com>
2025-03-27 10:47:51 +0545

    signup function created

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,7 @@ export default function RootLayout({
         className={` antialiased`}
       >
         <Navbar />
+        <Toaster richColors position="top-right" />
         <main>
           {children}
         </main>
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -18,10 +18,12 @@ import {
    FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client"
 import { formSchema } from "@/lib/auth-schema"
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { toast } from "sonner"
 import { z } from "zod"
 
 
@@ -35,7 +37,24 @@ export default function SignUpForm() {
       },
    })
 
-   function onSubmit(values: z.infer<typeof formSchema>) {
+   async function onSubmit(values: z.infer<typeof formSchema>) {
+      const { name, email, password } = values;
+      const { data, error } = await authClient.signUp.email({
+         email,
+         password,
+         name,
+         callbackURL: "/sign-in"
+      }, {
+         onRequest: (ctx) => {
+            toast("Signing up...")
+         },
+         onSuccess: (ctx) => {
+            form.reset()
+         },
+         onError: (ctx) => {
+            toast.error(ctx.error.message);
+         },
+      });
       console.log(values)
    }
 
