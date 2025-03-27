bd8fe1777acc2581fd6f73b17089d9126cd4bc87
Aayush <aausgh@gmail.com>
2025-03-27 14:23:00 +0545

    fix

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/sidebar"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import SignOutForm from "./sign-out-form"
+import type { User } from "@/lib/types"
 
 
 type UserProps = {
